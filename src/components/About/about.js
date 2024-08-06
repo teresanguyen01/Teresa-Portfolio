f54f2630dde18d093058589d8c52f2225d06084e
Teresa Nguyen <teresa.nguyen@yale.edu>
2024-08-05 22:13:52 -0400

    change descriptions

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -39,7 +39,7 @@ const About = () => {
         <div className="aboutDesc fadeElement">
           <p>Hello! My name is Teresa, and I'm currently a student at Yale, originally from Southern California, studying statistics, data science, and computer science. I have a passion for building programs that enhance efficiency and automation.</p>
           <p>Previously, I worked in cancer biology and nano-materials laboratories, focusing on carbon quantum dot synthesis to improve silicone polymers in manufacturing, and exploring the correlations between XIST genes and colorectal cancer to study gender differences. Although I found these fields fascinating, I discovered a deeper interest in the computational and analytical aspects, leading me to pivot towards computer science.</p>
-          <p>My goal is to leverage my computational skills to make a positive impact on society, particularly in healthcare technology. In my free time, I enjoy reading mystery novels, rock climbing, and cooking with hopes of taking my family to Flavortown someday!</p>
+          <p>My goal is to leverage my computational skills to make a positive impact on society. In my free time, I enjoy reading mystery novels, rock climbing, and cooking with hopes of taking my family to Flavortown someday!</p>
         </div>
       </div>
     </div>
