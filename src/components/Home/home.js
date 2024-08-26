31e8864aa422510720ea31345ee976ab623bb16d
Teresa Nguyen <teresa.nguyen@yale.edu>
2024-08-26 15:28:59 -0400

    editing resume

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -18,7 +18,7 @@ const Home = () => {
         <div className="introPara fadeIn">
           I currently am a <span className="highlightColor">Yale</span> student studying statistics, data science, and computer science. My interests lie in healthcare technology and technology in general, where I aim to leverage engineering to <span className="highlightColor">enhance</span> and <span className="highlightColor">transform</span> lives.
         </div>
-        <a href="https://drive.google.com/file/d/1pQzWNzZ8uxveQasjbXoQbroWM7fx2vPa/view?usp=sharing" className="btn">Resume</a>
+        <a href="https://drive.google.com/file/d/1oLyZ9BP4R4KM6D5bSzh1pbukR3c462dS/view?usp=sharing" className="btn">Resume</a>
       </div>
     </section>
   );
